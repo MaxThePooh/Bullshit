03ce3316149f69ae39cbfd893f220209bd3344df
MaxThePooh <67865666+MaxThePooh@users.noreply.github.com>
2023-02-08 09:56:43 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -26,5 +26,12 @@ def f23(x,y,z):
             if a==y: count+=1
     return count
                 
+def f23(x,y):
+    if x>y or x==17:
+        return 0
+    elif x==y:
+        return 1
+    return f(x+1,y) +f(x*2,y)
+#print(f(1,10)*f(10,35))
 #print(f23(1,10,10)*f23(10,35,25))
 
