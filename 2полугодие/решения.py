b49c8dccd68aaae812699acda57b09d8782864c2
MaxThePooh <67865666+MaxThePooh@users.noreply.github.com>
2023-02-08 09:35:32 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -8,3 +8,21 @@ def f5():
     if int(b,2)>516:
         print(N)
         break
+from itertools import product
+def f23(x,y,z):
+    count=0
+    for i in range(1,z):
+        nums=product('12',repeat=i)
+        for numb in nums:
+            a=x
+            if x==10 and numb.count('2')>1:continue
+            for ii in numb:
+                if a==17: break 
+                if ii=='1':a+=1
+                elif ii=='2' :a*=2
+
+            if a==y: count+=1
+    return count
+                
+#print(f23(1,10,10)*f23(10,35,25))
+
