d886d7b5bce082987640b719c9d37e973225a6ad
MaxThePooh <67865666+MaxThePooh@users.noreply.github.com>
2023-02-15 09:15:00 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -66,3 +66,21 @@ for n in nums:
             k+=1
 print(k)
 
+def f16()
+    import sys
+  sys.setrecursionlimit(3000)
+  def f(n):
+      if n==1:
+          return 1
+      else:
+          return n*f(n-1)
+  print(f(2023)/f(2020))
+
+
+
+  itog1=itog2=1
+  for x1 in range(1,2024):
+      itog1=itog1*x1
+  for x2 in range(1,2021):
+      itog2=itog2*x2
+  print(itog1/itog2)
