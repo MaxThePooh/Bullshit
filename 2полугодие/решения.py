8148bfce56eb5a9daad0ddb0aea0d708ae5044d0
MaxThePooh <67865666+MaxThePooh@users.noreply.github.com>
2023-02-21 05:53:28 +0300

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -33,6 +33,27 @@ def f23(x,y):
         return 1
     return f(x+1,y) +f(x*2,y)
 #print(f(1,10)*f(10,35))
+
+def f17():
+   with open('17.txt') as f:
+    nums=[int(x) for x in f]
+    c=list(map(abs,nums))
+    a=[]
+    for i in range(len(c)-1):
+        if c[i]%10==3:
+            a.append(c[i])
+    m=max(a)
+    count=0
+    summ=[]
+    #for i in range(len(nums)-1):
+       #if((nums[i]%10==3 and nums[i+1]%10!=3) or (nums[i]%10!=3 and nums[i+1]%10==3)):
+    for i in range(len(c)-1):
+        if((c[i]%10==3 and c[i+1]%10!=3) or (c[i]%10!=3 and c[i+1]%10==3)):
+            if nums[i]**2+nums[i+1]**2>=m**2:
+                count+=1
+                summ.append(nums[i]**2+nums[i+1]**2)
+    print(count)
+    print(max(summ))
 #print(f23(1,10,10)*f23(10,35,25))
 
 count=0
