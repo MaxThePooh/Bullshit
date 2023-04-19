dd7ffb657b1f4715c22ba2ebc181f9841a85972b
MaxThePooh <67865666+MaxThePooh@users.noreply.github.com>
2023-04-19 10:03:48 +0700

    Update решения.py

--- 2полугодие/решения.py
+++ 2полугодие/решения.py
@@ -161,3 +161,29 @@ def f16()
   for x2 in range(1,2021):
       itog2=itog2*x2
   print(itog1/itog2)
+
+  
+  
+  def f27():  
+  from math import ceil
+
+  f = open("27_B.txt")
+  n = int(f.readline())
+  elems = []
+  sum = 0
+  rightSum = 0
+  leftSum = 0
+  for i in range(0, n):
+      a, b = map(int, f.readline().split())
+      elems.append([a, ceil(b / 36)])
+  cost = [0] * n
+  for i in range(1, n):
+      cost[0] += (elems[i][0] - elems[0][0]) * elems[i][1]
+      rightSum += elems[i][1]
+  for i in range(1, n):
+      leftSum += elems[i - 1][1]
+      cost[i] = cost[i - 1] - rightSum * (elems[i][0] - elems[i - 1][0]) + leftSum * (elems[i][0] - elems[i - 1][0])
+      rightSum -= elems[i][1]
+  print(min(cost))
+  
+  
